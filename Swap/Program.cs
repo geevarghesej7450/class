15faac85379649e0052456e98f682d492024fcb1
geevarghesej7450 <geevarghesej7450@uhcl.edu>
2020-02-24 15:13:50 -0600

    code

---
+++ Swap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            Console.WriteLine($"{a} - {b}");
+            Swap(ref a, ref b);
+            Console.WriteLine($"{a} - {b}");
+        }
+        static void Swap(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
